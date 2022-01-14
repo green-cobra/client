719e2d1a789da19fde1aaee6eed98c04bb664ec0
Maksim Berezovskiy <mberezovskiy.96@gmail.com>
2022-01-14 11:27:20 +0300

    fix: debug params were passing to arguments

--- GreenCobra.Client.Console/Program.cs
+++ GreenCobra.Client.Console/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.CommandLine;
 using GreenCobra.Client.Console;
 
-var debugParams = $"green-cobra --local-port 57678";
+//var debugParams = $"green-cobra --local-port 57678";
 
 var bootstrap = new GreenCobraRootCommand();
 
-await bootstrap.InvokeAsync(debugParams /*args*/);
+await bootstrap.InvokeAsync(args);
